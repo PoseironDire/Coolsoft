7fc708f4a5e6e6c007831b1fe5d7a4e360378845
PoseironDire <poseirondire@gmail.com>
2021-04-16 16:29:04 +0200

    Changed Header Links

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
-function hideItQuick(){
-    document.querySelector(".tableon").classList.toggle("hidden");
+function hideItQuick() {
+  document.querySelector(".quickOn").classList.toggle("hidden");
 }
-function hideItSchedule(){
-    document.querySelector(".tableon").classList.toggle("hidden");
+function hideItSchedule() {
+  document.querySelector(".scheduleOn").classList.toggle("hidden");
 }
-function hideItCourses(){
-    document.querySelector(".tableon").classList.toggle("hidden");
+function hideItCourses() {
+  document.querySelector(".coursesOn").classList.toggle("hidden");
 }
-function hideItTeaching(){
-    document.querySelector(".tableon").classList.toggle("hidden");
+function hideItTeaching() {
+  document.querySelector(".teachingOn").classList.toggle("hidden");
 }
-function hideItDocument(){
-    document.querySelector(".tableon").classList.toggle("hidden");
+function hideItDocument() {
+  document.querySelector(".documentOn").classList.toggle("hidden");
 }
-function hideItPresense(){
-    document.querySelector(".tableon").classList.toggle("hidden");
+function hideItPresense() {
+  document.querySelector(".presenseOn").classList.toggle("hidden");
 }
-function hideItLinks(){
-    document.querySelector(".tableon").classList.toggle("hidden");
+function hideItLinks() {
+  document.querySelector(".linksOn").classList.toggle("hidden");
 }
