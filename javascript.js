15e599f276c7de33d5040b434f76440f5647b77a
Markus <Markuspiotrowski@live.com>
2021-04-16 14:44:19 +0200

    Java Fix

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
-function hideIt(){
+function hideItQuick(){
+    document.querySelector(".tableon").classList.toggle("hidden");
+}
+function hideItSchedule(){
+    document.querySelector(".tableon").classList.toggle("hidden");
+}
+function hideItCourses(){
+    document.querySelector(".tableon").classList.toggle("hidden");
+}
+function hideItTeaching(){
+    document.querySelector(".tableon").classList.toggle("hidden");
+}
+function hideItDocument(){
+    document.querySelector(".tableon").classList.toggle("hidden");
+}
+function hideItPresense(){
+    document.querySelector(".tableon").classList.toggle("hidden");
+}
+function hideItLinks(){
     document.querySelector(".tableon").classList.toggle("hidden");
 }
